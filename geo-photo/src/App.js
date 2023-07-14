66077c6df0be699d5a9f9323d9b0a9e32011c1b0
Hector Flores <106941903+hekmaflo@users.noreply.github.com>
2023-07-13 23:16:26 -0500

    user page created

--- geo-photo/src/App.js
+++ geo-photo/src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import Users from "./user/pages/Users";
 
 function App() {
-  return <h1>Let's start!</h1>;
+  return (
+    <Router>
+      <Route path="/">
+        <Users />
+      </Route>
+    </Router>
+  );
 }
 
 export default App;
