b68af2b1ac3fac31faa85a0d0413a1e63593497b
Hector Flores <106941903+hekmaflo@users.noreply.github.com>
2023-07-15 15:20:22 -0500

    added shared ui elements

--- geo-photo/src/user/pages/Users.js
+++ geo-photo/src/user/pages/Users.js
@@ -10,13 +10,6 @@ const Users = () => {
         "https://images.pexels.com/photos/17576069/pexels-photo-17576069/free-photo-of-diving-under-water-freediving-underwater-photography.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
       places: 3,
     },
-    {
-      id: "u1",
-      name: "joma tech",
-      image:
-        "https://images.pexels.com/photos/17576069/pexels-photo-17576069/free-photo-of-diving-under-water-freediving-underwater-photography.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-      places: 4,
-    },
   ];
 
   return <UsersList items={USERS} />;
