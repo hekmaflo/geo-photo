ba0f3873d2faffced515e48be78ebe5d9fa0fe58
Hector Flores <106941903+hekmaflo@users.noreply.github.com>
2023-07-15 13:42:34 -0500

    added style to user item and user list components

--- geo-photo/src/user/pages/Users.js
+++ geo-photo/src/user/pages/Users.js
@@ -1,7 +1,25 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <h2>User Works!</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Hector Flores",
+      image:
+        "https://images.pexels.com/photos/17576069/pexels-photo-17576069/free-photo-of-diving-under-water-freediving-underwater-photography.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      places: 3,
+    },
+    {
+      id: "u1",
+      name: "joma tech",
+      image:
+        "https://images.pexels.com/photos/17576069/pexels-photo-17576069/free-photo-of-diving-under-water-freediving-underwater-photography.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      places: 4,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
