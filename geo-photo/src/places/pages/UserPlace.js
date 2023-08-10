90c47ccdd529ae6e99f24b13a94416aa3c41f1f5
Hector <hmflores88@gmail.com>
2023-08-10 12:25:44 -0500

    added dummy data to test place list component

--- geo-photo/src/places/pages/UserPlace.js
+++ geo-photo/src/places/pages/UserPlace.js
@@ -1,7 +1,37 @@
 import React from 'react'
 
+import PlaceList from '../components/PlaceList';
+
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Empire State Building',
+        description: 'One of the most famous sky scrapers in the world',
+        imageUrl: 'https://s39023.pcdn.co/wp-content/uploads/2022/10/Where-Are-Those-Morgans-Empire-State-Building-728x546.jpg.optimal.jpg',
+        address: '20 W 34th St, New York, NY 10001',
+        location: {
+            lat: 40.7484405,
+            lng: -73.9878584
+        },
+        creator: 'u1'
+    },
+
+    {
+        id: 'p2',
+        title: 'Empire State Building',
+        description: 'One of the most famous sky scrapers in the world',
+        imageUrl: 'https://s39023.pcdn.co/wp-content/uploads/2022/10/Where-Are-Those-Morgans-Empire-State-Building-728x546.jpg.optimal.jpg',
+        address: '20 W 34th St, New York, NY 10001',
+        location: {
+            lat: 40.7484405,
+            lng: -73.9878584
+        },
+        creator: 'u2'    
+    }
+];
+
 const UserPlace = () => {
-    return
+    return <PlaceList items={DUMMY_PLACES} />
 };
 
 export default UserPlace;
