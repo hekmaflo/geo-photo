3af18eec2232823478d4ad2472cc9455fcf20a9c
Hector <hmflores88@gmail.com>
2023-08-10 10:00:42 -0500

    added place item component

--- geo-photo/src/places/components/PlaceItem.js
+++ geo-photo/src/places/components/PlaceItem.js
@@ -3,7 +3,16 @@ import React from "react";
 import './PlaceItem.css';
 
 const PlaceItem = props =>  {
-    return
+    return <li className="place-item">
+        <div className="place-item__img">
+            <img src={props.image} alt={props.title}/>
+            </div>
+            <div className="place-item__info">
+                <h2>{props.title}</h2>
+                <h3>{props.address}</h3>
+                <p>{props.description}</p>
+            </div>
+    </li>
 };
 
 export default PlaceItem;
--- geo-photo/src/places/components/PlaceList.js
+++ geo-photo/src/places/components/PlaceList.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Card from "../../shared/components/UIElements/Card";
+import PlaceItem from "./PlaceItem";
 import './PlaceList.css';
 
 const PlaceList = props =>{
@@ -13,6 +14,17 @@ const PlaceList = props =>{
         </div>
         );
     }
+
+    return <ul className="place-list">
+        {props.items.map(place => <PlaceItem key={place.id} 
+        id={place.id} 
+        image={place.imageUrl} 
+        title={place.title} 
+        description={place.description} 
+        address={place.address}
+        creatorId={place.creator}
+        coordinates={place.location}/>)}
+    </ul>
 };
 
 export default PlaceList;
